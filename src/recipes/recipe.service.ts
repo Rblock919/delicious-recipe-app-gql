d9bad184e5bafff4400a05bf99572ac22764d664
Ryan Block <Ryan.Block93@gmail.com>
2020-06-13 00:11:02 -0400

    moved all functionality from recipeResolvers to recipeService (still need to test most methods). created unit tests for all resolver functions (still need to create unit tests for raters and comments field resolvers

--- src/recipes/recipe.service.ts
+++ src/recipes/recipe.service.ts
@@ -1,3 +1,7 @@
+import { UserInputError } from 'apollo-server';
+
+// TODO: improve all methods with try-catches where relevant
+
 export class RecipeService {
   constructor() {
     console.log('in recipe service constructor');
@@ -17,5 +21,108 @@ export class RecipeService {
 
   async getUnapprovedRecipeById(recipeId, newRecipeLoader): Promise<any> {
     return newRecipeLoader.load(recipeId);
+  }
+
+  // TODO: TEST ALL THE BELOW FUNCTIONS NOW THAT THEY HAVE BEEN MOVED HERE
+  // TODO: improve the below two methods with try-catches
+  async deleteRecipe(recipeId, recipeModel): Promise<string> {
+    await recipeModel.findByIdAndDelete(recipeId);
+    return 'Success';
+  }
+
+  async rejectRecipe(approvalId, newRecipeModel): Promise<string> {
+    await newRecipeModel.findByIdAndDelete(approvalId);
+    return 'Success';
+  }
+
+  async rateRecipe(recipeId, rating, recipeModel, userId): Promise<string> {
+    const recipe = await recipeModel.findById(recipeId);
+    const { raters } = recipe;
+
+    raters.set(userId, rating);
+
+    await recipeModel.updateOne(
+      { _id: recipeId },
+      { $set: { raters } },
+      { upsert: true, new: true }
+    );
+
+    return 'Success';
+  }
+
+  async favoriteRecipe(recipeId, recipeModel, userId): Promise<string> {
+    const recipe = await recipeModel.findById(recipeId);
+    const { favoriters } = recipe;
+    let newFavoriters: string[];
+
+    if (favoriters.includes(userId)) {
+      newFavoriters = favoriters.filter((x: string) => x !== userId);
+    } else {
+      newFavoriters = favoriters;
+      newFavoriters.push(userId);
+    }
+
+    await recipeModel.updateOne(
+      { _id: recipeId },
+      { $set: { favoriters: newFavoriters } },
+      { upsert: true, new: true }
+    );
+    return 'Success';
+  }
+
+  async updateRecipe(recipe, recipeId, recipeModel): Promise<any> {
+    const updatedData = {
+      title: recipe.title,
+      producer: recipe.producer,
+      ingredients: recipe.ingredients,
+      steps: recipe.steps,
+      imgDir: recipe.imgDir,
+      // TODO: fix the discrepancy in the angular forms between nutrition & nutritionValues
+      nutritionValues: recipe.nutrition,
+    };
+
+    return recipeModel.findByIdAndUpdate(
+      recipeId,
+      { $set: updatedData },
+      {
+        new: true,
+      }
+    );
+  }
+
+  async submitRecipeForApproval(input, NewRecipeModel): Promise<string> {
+    const recipe = input;
+    recipe.nutritionValues = { ...recipe.nutrition };
+    delete recipe.nutrition;
+
+    const newRecipe = new NewRecipeModel(recipe);
+
+    // Since this value is an array in mongo it will autopopulate and therefor cannot implement reqruied at a db level
+    if (newRecipe.producer !== 'Blue Apron' && newRecipe.preCook.length === 0) {
+      throw new UserInputError(
+        'Home Chef & Hello Fresh Recipes Must Have a Precook Value'
+      );
+    }
+
+    await newRecipe.save();
+    return 'Success';
+  }
+
+  async approveRecipe(
+    approvalId,
+    inputRecipe,
+    RecipeModel,
+    newRecipeModel
+  ): Promise<any> {
+    const recipe = inputRecipe;
+    recipe.nutritionValues = { ...recipe.nutrition };
+    delete recipe.nutrition;
+
+    const newRecipe = new RecipeModel(recipe);
+
+    await newRecipe.save();
+    await newRecipeModel.findByIdAndDelete(approvalId);
+
+    return newRecipe;
   }
 }
--- src/recipes/tests/recipeResolvers.spec.ts
+++ src/recipes/tests/recipeResolvers.spec.ts
@@ -8,15 +8,28 @@ describe('RecipeResolvers', () => {
     let id;
     let loaders;
     let models;
+    let user;
 
     beforeEach(() => {
       id = 'testId';
+      user = {
+        username: 'dev',
+        _id: 'userId',
+        isAdmin: true,
+      };
       loaders = { Recipe: { load: jest.fn() }, NewRecipe: { load: jest.fn() } };
       recipeService = {
         getAllRecipes: jest.fn(),
         getRecipeById: jest.fn(),
         getAllUnapprovedRecipes: jest.fn(),
         getUnapprovedRecipeById: jest.fn(),
+        deleteRecipe: jest.fn(),
+        rejectRecipe: jest.fn(),
+        rateRecipe: jest.fn(),
+        favoriteRecipe: jest.fn(),
+        updateRecipe: jest.fn(),
+        submitRecipeForApproval: jest.fn(),
+        approveRecipe: jest.fn(),
       };
       services = { Recipe: recipeService };
       models = {
@@ -76,6 +89,122 @@ describe('RecipeResolvers', () => {
       expect(recipeService.getUnapprovedRecipeById).toHaveBeenCalledWith(
         id,
         loaders.NewRecipe
+      );
+    });
+
+    it('should call deleteRecipe from recipeService on delete mutation with the correct params', () => {
+      recipeResolvers.Mutation.delete(
+        null,
+        { id },
+        {
+          services,
+          models,
+        }
+      );
+      expect(recipeService.deleteRecipe).toHaveBeenCalledWith(
+        id,
+        models.Recipe
+      );
+    });
+
+    it('should call rejectRecipe from recipeService on reject mutation with the correct params', () => {
+      recipeResolvers.Mutation.reject(
+        null,
+        { id },
+        {
+          services,
+          models,
+        }
+      );
+      expect(recipeService.rejectRecipe).toHaveBeenCalledWith(
+        id,
+        models.NewRecipe
+      );
+    });
+
+    it('should call rateRecipe from recipeService on rate mutation with the correct params', () => {
+      recipeResolvers.Mutation.rate(
+        null,
+        { input: { recipeId: id, rating: 3 } },
+        {
+          services,
+          models,
+          user,
+        }
+      );
+      expect(recipeService.rateRecipe).toHaveBeenCalledWith(
+        id,
+        3,
+        models.Recipe,
+        user._id
+      );
+    });
+
+    it('should call favoriteRecipe from recipeService on favorite mutation with the correct params', () => {
+      recipeResolvers.Mutation.favorite(
+        null,
+        { id },
+        {
+          services,
+          models,
+          user,
+        }
+      );
+      expect(recipeService.favoriteRecipe).toHaveBeenCalledWith(
+        id,
+        models.Recipe,
+        user._id
+      );
+    });
+
+    it('should call updateRecipe from recipeService on update mutation with the correct params', () => {
+      const recipe = { data: 'yo' };
+      recipeResolvers.Mutation.update(
+        null,
+        { input: { recipe, recipeId: id } },
+        {
+          services,
+          models,
+        }
+      );
+      expect(recipeService.updateRecipe).toHaveBeenCalledWith(
+        recipe,
+        id,
+        models.Recipe
+      );
+    });
+
+    it('should call submitForApproval from recipeService on submit mutation with the correct params', () => {
+      const input = { data: 'yo' };
+      recipeResolvers.Mutation.submit(
+        null,
+        { input },
+        {
+          services,
+          models,
+        }
+      );
+      expect(recipeService.submitRecipeForApproval).toHaveBeenCalledWith(
+        input,
+        models.NewRecipe
+      );
+    });
+
+    it('should call approveRecipe from recipeService on approve mutation with the correct params', () => {
+      const recipe = { data: 'yo' };
+      recipeResolvers.Mutation.approve(
+        null,
+        { input: { approvalId: id, recipe } },
+        {
+          services,
+          models,
+        }
+      );
+      expect(recipeService.approveRecipe).toHaveBeenCalledWith(
+        id,
+        recipe,
+        models.Recipe,
+        models.NewRecipe
       );
     });
   });
